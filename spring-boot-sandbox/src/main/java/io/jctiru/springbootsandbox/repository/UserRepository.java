5bd646e826b6437da7ca9914df187318fef7abb9
jctiru <jonathanchristiantiru@gmail.com>
2019-11-01 12:54:58 +1000

    Create user repository

---
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package io.jctiru.springbootsandbox.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.jctiru.springbootsandbox.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
