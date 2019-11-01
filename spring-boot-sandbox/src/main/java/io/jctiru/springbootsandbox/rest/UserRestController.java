f7767b9bd65757d0bee0a08a39c8e687e04cf9d6
jctiru <jonathanchristiantiru@gmail.com>
2019-11-01 15:03:50 +1000

    Update create user method in user controller

--- spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/rest/UserRestController.java
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/rest/UserRestController.java
@@ -1,15 +1,26 @@
 package io.jctiru.springbootsandbox.rest;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jctiru.springbootsandbox.dto.UserDto;
+import io.jctiru.springbootsandbox.model.request.UserDetailsRequestModel;
+import io.jctiru.springbootsandbox.model.response.UserRest;
+import io.jctiru.springbootsandbox.service.UserService;
 
 @RestController
 @RequestMapping("users")
 public class UserRestController {
+	
+	@Autowired
+	UserService userService;
 
 	@GetMapping
 	public String getUser() {
@@ -17,8 +28,16 @@ public class UserRestController {
 	}
 
 	@PostMapping
-	public String createUser() {
-		return "Create user called";
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		return returnValue;
 	}
 
 	@PutMapping
