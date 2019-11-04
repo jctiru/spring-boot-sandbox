7cb7505b604a74b3108515f8095120b969be223f
jctiru <jonathanchristiantiru@gmail.com>
2019-11-04 23:34:57 +1000

    Implement handling specific exception

---
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/exception/AppExceptionsHandler.java
@@ -0,0 +1,23 @@
+package io.jctiru.springbootsandbox.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import io.jctiru.springbootsandbox.ui.model.response.ErrorMessage;
+
+@ControllerAdvice
+public class AppExceptionsHandler {
+
+	@ExceptionHandler(value = { UserServiceException.class })
+	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+		return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
