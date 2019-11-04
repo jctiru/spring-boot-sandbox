90f88e50d7974698042a503360fdae22adeb75f1
jctiru <jonathanchristiantiru@gmail.com>
2019-11-04 19:29:11 +1000

    Transfer token secret to properties file

--- spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/security/SecurityConstants.java
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/security/SecurityConstants.java
@@ -1,11 +1,16 @@
 package io.jctiru.springbootsandbox.security;
 
+import io.jctiru.springbootsandbox.SpringApplicationContext;
+
 public class SecurityConstants {
 
 	public static final long EXPIRATION_TIME = 864000000L; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "jWR0Acsg4ArKngU";
 
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		return appProperties.getTokenSecret();
+	}
 }
