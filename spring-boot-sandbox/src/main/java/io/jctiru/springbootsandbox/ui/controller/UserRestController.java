2e4358877211d634d42371e4f8d853b237310966
jctiru <jonathanchristiantiru@gmail.com>
2019-11-25 18:02:14 +1000

    Use model mapper

--- spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/ui/controller/UserRestController.java
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/ui/controller/UserRestController.java
@@ -3,6 +3,7 @@ package io.jctiru.springbootsandbox.ui.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -66,8 +67,11 @@ public class UserRestController {
 
 		UserRest returnValue = new UserRest();
 
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
+		// UserDto userDto = new UserDto();
+		// BeanUtils.copyProperties(userDetails, userDto);
+
+		ModelMapper modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 
 		UserDto createdUser = userService.createUser(userDto);
 		BeanUtils.copyProperties(createdUser, returnValue);
