50e579824b2704f50445f8d6aeb6e1218c5f38a7
jctiru <jonathanchristiantiru@gmail.com>
2020-01-07 17:44:33 +1000

    Add address entity

--- spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/io/entity/AddressEntity.java
+++ spring-boot-sandbox/src/main/java/io/jctiru/springbootsandbox/io/entity/AddressEntity.java
@@ -1,15 +1,45 @@
-package io.jctiru.springbootsandbox.shared.dto;
+package io.jctiru.springbootsandbox.io.entity;
 
-public class AddressDto {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "address")
+public class AddressEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private long id;
+
+	@Column(name = "address_id")
 	private String addressId;
+
+	@Column(name = "city")
 	private String city;
+
+	@Column(name = "country")
 	private String country;
+
+	@Column(name = "street_name")
 	private String streetName;
+
+	@Column(name = "postal_code")
 	private String postalCode;
+
+	@Column(name = "type")
 	private String type;
-	private UserDto userDetails;
+
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+	@JoinColumn(name = "users_id")
+	private UserEntity user;
 
 	public long getId() {
 		return id;
@@ -67,12 +97,12 @@ public class AddressDto {
 		this.type = type;
 	}
 
-	public UserDto getUserDetails() {
-		return userDetails;
+	public UserEntity getUser() {
+		return user;
 	}
 
-	public void setUserDetails(UserDto userDetails) {
-		this.userDetails = userDetails;
+	public void setUser(UserEntity user) {
+		this.user = user;
 	}
 
 }
